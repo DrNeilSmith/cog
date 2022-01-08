7fc6ee4790ab68f22828dd5550a616ac8a3c3423
Neil A Smith <DrNeilSmith@users.noreply.github.com>
2022-01-08 17:41:30 +0000

    Try with updated URL

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 setup(name='cogdb',
       version='3.0.1',
       description='Persistent Embedded Graph Database',
-      url='http://github.com/arun1729/cog',
+      url='https://github.com/DrNeilSmith/cog/',
       author='Arun Mahendra',
       author_email='arunm3.141@gmail.com',
       license='MIT',
