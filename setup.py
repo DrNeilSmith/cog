97e99fa1717075b69b2f5ff16547980a24e28479
Neil A Smith <DrNeilSmith@users.noreply.github.com>
2022-01-08 17:40:15 +0000

    Update xxhash via setup

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ setup(name='cogdb',
       author_email='arunm3.141@gmail.com',
       license='MIT',
       packages=['cog'],
-      install_requires=['xxhash==2.0.0'],
+      install_requires=['xxhash==2.0.2'],
       zip_safe=False)
